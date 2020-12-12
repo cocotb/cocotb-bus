b46c6ab40c9e502647af89a16b8108de9764f76a
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-12-12 17:52:27 -0600

    Delete all sources we know we won't keep

--- documentation/source/genindex.rst
+++
@@ -1,7 +0,0 @@
-..
-   This file is a placeholder and will be replaced by Sphinx
-   See https://stackoverflow.com/a/42310803
-
-*****
-Index
-*****
--- tests/test_cases/test_exit_error/test_exit.py
+++
@@ -1,9 +0,0 @@
-import cocotb
-from cocotb.triggers import Timer
-
-
-@cocotb.test()
-async def typosyntax_error():
-    # this syntax error makes the whole file unimportable, so the file contents
-    # don't really matter.
-    await Timer(100)a  # noqa
