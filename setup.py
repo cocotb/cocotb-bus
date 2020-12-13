529e92319ef9d23450c65166e2719797daea7c06
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-12-12 18:19:30 -0600

    Add enough to make cocotb-bus installable

---
+++ setup.py
@@ -0,0 +1,31 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from setuptools import setup, find_packages
+
+
+with open("README.md") as file:
+    long_description = file.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="cocotb-bus",
+        use_scm_version=dict(
+            write_to='src/cocotb_bus/_version.py',
+            version_scheme='release-branch-semver'
+        ),
+        author="cocotb maintainers",
+        author_email="cocotb@lists.librecores.org",
+        description="",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/cocotb/cocotb-bus",
+        packages=find_packages("src"),
+        package_dir={"": "src"},
+        install_requires=[
+            "cocotb>1.4,<2.0"
+        ],
+        python_requires='>=3.5'
+    )
---
+++ src/cocotb_bus/__init__.py
@@ -0,0 +1,5 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from ._version import version as __version__  # noqa
