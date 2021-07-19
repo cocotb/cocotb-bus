8fedb660d7112a0a37246a1e97b93c5a72ebf69e
Martijn Bastiaan <martijn@hmbastiaan.nl>
2021-07-19 15:41:36 +0200

    Set __version__ to 'dev' if no _version module found

--- src/cocotb_bus/__init__.py
+++ src/cocotb_bus/__init__.py
@@ -2,4 +2,7 @@
 # Licensed under the Revised BSD License, see LICENSE for details.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from ._version import version as __version__  # noqa
+try:
+	from ._version import version as __version__  # noqa
+except ModuleNotFoundError:
+	__version__ = 'dev'
